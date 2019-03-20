68f2945937f450c96afb4fedf682bd31bdbfce62
arthur.yueh <ysde108@gmail.com>
2019-03-20 11:13:41 +0800

    set search limit default to 100000

--- grafanaSettings.py
+++ grafanaSettings.py
@@ -4,4 +4,5 @@ grafana_url = os.getenv('GRAFANA_URL', 'http://localhost:3000')
 token = os.getenv('GRAFANA_TOKEN', '')
 http_get_headers = {'Authorization': 'Bearer ' + token}
 http_post_headers = {'Authorization': 'Bearer ' + token, 'Content-Type': 'application/json'}
+search_api_limit = 100000
 debug = False
