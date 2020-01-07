1e2a4661bd9fd716376b2d7c872fd35754258129
Stanislasss <10722455+Stanislasss@users.noreply.github.com>
2020-01-07 11:58:11 +0100

    changes for S3 upload

--- backup_grafana.sh
+++ backup_grafana.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-set -e
+set -e -x
 
 trap 'echo -ne "\n:::\n:::\tCaught signal, exiting at line $LINENO, while running :${BASH_COMMAND}:\n:::\n"; exit' SIGINT SIGQUIT
 
 current_path=$(pwd)
 backup_dir="_OUTPUT_"
 timestamp=$(date +"%Y-%m-%dT%H-%M-%S")
+BACKUP_FOLDER="${CLUSTER_NAME}"
+if [ -n "${GRAFANA_SERVICE_PORT}" ]; then
+	GRAFANA_URL="${GRAFANA_URL}:${GRAFANA_SERVICE_PORT}"
+fi
 
 [ -d "${backup_dir}" ] || mkdir -p "${backup_dir}"
 
@@ -19,3 +23,5 @@ done
 
 tar -czvf "${backup_dir}/${timestamp}.tar.gz" ${backup_dir}/{dashboards,datasources,folders,alert_channels}/${timestamp}
 rm -rf ${backup_dir}/{dashboards,datasources,folders,alert_channels}/${timestamp}
+echo "Uploading grafana backup to S3 bucket ${S3_BACKUP}"
+aws s3 cp ./"${backup_dir}"/"${timestamp}".tar.gz s3://"${S3_BACKUP}"/"${BACKUP_FOLDER}"/"${timestamp}".tar.gz
