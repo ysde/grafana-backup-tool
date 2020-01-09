5bb842f6bfe725c986ae0d6e3bcea1e764d68dd9
jboulanger <julien.boulanger@fr.clara.net>
2020-01-09 17:38:34 +0100

    fix(casting): cast int to str

--- src/save_alert_channels.py
+++ src/save_alert_channels.py
@@ -37,12 +37,12 @@ def get_indivisual_alert_channel_and_save(channels):
             
         save_alert_channel(
             to_python2_and_3_compatible_string(channel['name']),
-            to_python2_and_3_compatible_string(channel_identifier),
+            to_python2_and_3_compatible_string(str(channel_identifier)),
             channel
         )
         file_path = folder_path + '/' + log_file
         with open(u"{0}".format(file_path) , 'w+') as f:
-            f.write('{}\t{}'.format(to_python2_and_3_compatible_string(channel_identifier), to_python2_and_3_compatible_string(channel['name'])))
+            f.write('{}\t{}'.format(to_python2_and_3_compatible_string(str(channel_identifier)), to_python2_and_3_compatible_string(channel['name'])))
 
 
 alert_channels = get_all_alert_channels_in_grafana()
