f768e0a8f6ab9a05e7b99e5b2523df821731bcda
brandon-vargas <brandonvargas33873@yahoo.com>
2020-01-30 10:01:17 -0500

    Switching back to a updated dict comprehension that works.

--- src/grafanaSettings.py
+++ src/grafanaSettings.py
@@ -3,10 +3,7 @@ import os
 GRAFANA_URL = os.getenv('GRAFANA_URL', 'http://localhost:3000')
 TOKEN = os.getenv('GRAFANA_TOKEN', 'eyJrIjoiSkQ5NkdvWllHdnVNdlVhWUV3Tm5LSGc4NG53UFdSTjQiLCJuIjoiYWRtaW4iLCJpZCI6MX0=')
 
-if 'GRAFANA_HEADERS' in os.environ:
-    EXTRA_HEADERS = dict(h.split(':') for h in os.getenv('GRAFANA_HEADERS').split(','))
-else:
-    EXTRA_HEADERS = {}
+EXTRA_HEADERS = dict(h.split(':') for h in os.getenv('GRAFANA_HEADERS', '').split(',') if 'GRAFANA_HEADERS' in os.environ)
 
 HTTP_GET_HEADERS = {'Authorization': 'Bearer ' + TOKEN, **EXTRA_HEADERS}
 HTTP_POST_HEADERS = {'Authorization': 'Bearer ' + TOKEN, 'Content-Type': 'application/json', **EXTRA_HEADERS}
