b45b703cf3afbce88d583e80984cfa8ea009fbb9
arthur.yueh <arthur.yueh@oneplus.com>
2020-03-16 15:18:02 +0800

    fix issue:54 https://github.com/ysde/grafana-backup-tool/issues/54

--- src/grafanaSettings.py
+++ src/grafanaSettings.py
@@ -5,8 +5,12 @@ TOKEN = os.getenv('GRAFANA_TOKEN', 'eyJrIjoiSkQ5NkdvWllHdnVNdlVhWUV3Tm5LSGc4NG53
 
 EXTRA_HEADERS = dict(h.split(':') for h in os.getenv('GRAFANA_HEADERS', '').split(',') if 'GRAFANA_HEADERS' in os.environ)
 
-HTTP_GET_HEADERS = {'Authorization': 'Bearer ' + TOKEN, **EXTRA_HEADERS}
-HTTP_POST_HEADERS = {'Authorization': 'Bearer ' + TOKEN, 'Content-Type': 'application/json', **EXTRA_HEADERS}
+HTTP_GET_HEADERS = {'Authorization': 'Bearer ' + TOKEN}
+HTTP_POST_HEADERS = {'Authorization': 'Bearer ' + TOKEN, 'Content-Type': 'application/json'}
+for k,v in EXTRA_HEADERS.items():
+    HTTP_GET_HEADERS.update({k: v})
+    HTTP_POST_HEADERS.update({k: v})
+
 SEARCH_API_LIMIT = 5000
 DEBUG = True
 VERIFY_SSL = False
