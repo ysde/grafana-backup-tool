ef689ea8bd760affdc073034a9ab03218a5d3ddd
Aaron Johnson <aaron.johnson@crowdstrike.com>
2020-07-16 16:05:52 -0500

    added refactored backup/restore wrapper shell scripts

---
+++ backup_grafana.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+trap 'echo -ne "\n:::\n:::\tCaught signal, exiting at line $LINENO, while running :${BASH_COMMAND}:\n:::\n"; exit' SIGINT SIGQUIT
+
+settings_file="${1:-grafanaSettings.json}"
+backup_dir="_OUTPUT_"
+
+if [[ ! -f "${settings_file}" ]]; then
+	echo "Usage:"
+	echo -e "\t$0 <path_to_settings_file>"
+	echo -e "\te.g. $0 '/path/to/grafanaSettings.json'"
+	exit 1
+fi
+
+grafana-backup --config $settings_file save
---
+++ restore_grafana.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+trap 'echo -ne "\n:::\n:::\tCaught signal, exiting at line $LINENO, while running :${BASH_COMMAND}:\n:::\n"; exit' SIGINT SIGQUIT
+
+archive_file="$1"
+settings_file="${2:-grafanaSettings.json}"
+
+if [[ ! -f "${archive_file}" || ! -f "${settings_file}" ]]; then
+	echo "Usage:"
+	echo -e "\t$0 <path_to_archive_file> <path_to_settings_file>"
+	echo -e "\te.g. $0 '_OUTPUT_/2019-05-13T11-04-33.tar.gz' '/path/to/grafanaSettings.json'"
+	exit 1
+fi
+
+grafana-backup --config $settings_file restore $archive_file
