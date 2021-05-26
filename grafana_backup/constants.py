8b55f2ea3fc325528ef5ef703f8de7488efd3ebc
Tommy Carpenter <tommy@merqube.com>
2021-05-26 19:47:59 -0400

    Changelog, vbump

--- grafana_backup/constants.py
+++ grafana_backup/constants.py
@@ -1,5 +1,11 @@
 import os
 
-PKG_NAME = 'grafana-backup'
-PKG_VERSION = '1.1.8'
-JSON_CONFIG_PATH = '{0}/.grafana-backup.json'.format(os.environ['HOME'])
+
+if os.name == "nt":
+    homedir = os.environ["HOMEPATH"]
+else:
+    homedir = os.environ["HOME"]
+
+PKG_NAME = "grafana-backup"
+PKG_VERSION = "1.1.8"
+JSON_CONFIG_PATH = "{0}/.grafana-backup.json".format(homedir)
