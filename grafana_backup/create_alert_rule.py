53c2293bf94200ca0bf38a7a0f99f318df574cc6
mt3593 <mtinsley@greshamtech.com>
2023-05-30 15:43:56 +0100

    Perform upsert of alert rules

--- grafana_backup/create_alert_rule.py
+++ grafana_backup/create_alert_rule.py
@@ -1,12 +1,12 @@
 import json
-from grafana_backup.dashboardApi import create_alert_rule, get_grafana_version
+from grafana_backup.dashboardApi import get_alert_rule, create_alert_rule, update_alert_rule, get_grafana_version
 from packaging import version
 
 
 def main(args, settings, file_path):
     grafana_url = settings.get('GRAFANA_URL')
     http_post_headers = settings.get('HTTP_POST_HEADERS')
-    http_post_headers['x-disable-provenance']='*'
+    http_get_headers = settings.get('HTTP_GET_HEADERS')
     verify_ssl = settings.get('VERIFY_SSL')
     client_cert = settings.get('CLIENT_CERT')
     debug = settings.get('DEBUG')
@@ -19,7 +19,15 @@ def main(args, settings, file_path):
 
     if minimum_version <= grafana_version:
         alert_rule = json.loads(data)
-        result = create_alert_rule(json.dumps(alert_rule), grafana_url, http_post_headers, verify_ssl, client_cert, debug)
+        del alert_rule['id']
+        uid = alert_rule['uid']
+        get_response= get_alert_rule(uid, grafana_url, http_get_headers, verify_ssl, client_cert, debug)
+        status_code=get_response[0]
+        print("Got a code: {0}", status_code)
+        if status_code == 404:
+           result = create_alert_rule(json.dumps(alert_rule), grafana_url, http_post_headers, verify_ssl, client_cert, debug)
+        else:
+           result = update_alert_rule(alert_rule['uid'], json.dumps(alert_rule), grafana_url, http_post_headers, verify_ssl, client_cert, debug)
         print("create alert rule: {0}, status: {1}, msg: {2}".format(alert_rule['title'], result[0], result[1]))
     else:
         print("Unable to create alert rules, requires Grafana version {0} or above. Current version is {1}".format(minimum_version, grafana_version))
