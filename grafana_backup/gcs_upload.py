56047e2ace0b67badd2ce0a49685f08ce9f5082c
Aleksei Balalaev <abalalaev@asteriosoft.com>
2024-03-07 12:34:34 +0700

    Added ability to specify a subfolder in the GCS bucket

--- grafana_backup/gcs_upload.py
+++ grafana_backup/gcs_upload.py
@@ -4,6 +4,7 @@ from google.cloud import storage
 
 def main(args, settings):
     bucket_name = settings.get('GCS_BUCKET_NAME')
+    bucket_path = settings.get('GCS_BUCKET_PATH').strip('/')
     backup_dir = settings.get('BACKUP_DIR')
     timestamp = settings.get('TIMESTAMP')
 
@@ -11,16 +12,17 @@ def main(args, settings):
 
     gcs_file_name = '{0}.tar.gz'.format(timestamp)
     archive_file = '{0}/{1}'.format(backup_dir, gcs_file_name)
+    gcs_blob_name = gcs_file_name if bucket_path == '' else '{0}/{1}'.format(bucket_path, gcs_file_name)
 
     try:
         bucket = storage_client.bucket(bucket_name)
 
-        blob = bucket.blob(gcs_file_name)
+        blob = bucket.blob(gcs_blob_name)
         blob.upload_from_filename(archive_file)
 
         print("Upload to gcs: was successful")
     except FileNotFoundError:  # noqa: F821
-        print("The file: {0} was not found".format(gcs_file_name))
+        print("The file: {0} was not found".format(archive_file))
         return False
     except api_core.exceptions.Forbidden as e:
         print("Permission denied: {0}, please grant `Storage Admin` to service account you used".format(str(e)))
